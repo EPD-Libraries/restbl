87d59866c4f97ef16bde6891a565dfe5a68c3081
ArchLeaders <archparliament@gmail.com>
2023-05-10 19:25:33 -0700

    Impl binary serializer

--- src/restbl.cpp
+++ src/restbl.cpp
@@ -10,6 +10,9 @@ RESTBL::RESTBL(tcb::span<const u8> data) : m_reader{data, exio::Endianness::Litt
     throw exio::InvalidDataError("Invalid RESTBL magic");
   }
 
+  m_unknown_1 = header.unknown_1;
+  m_unknown_2 = header.unknown_2;
+
   for (size_t i = 0; i < header.crc_table_num; i++) {
     const auto entry = *m_reader.Read<restbl::HashEntry>();
     m_crc_table.insert({entry.hash, entry.size});
@@ -26,8 +29,26 @@ RESTBL RESTBL::FromBinary(tcb::span<const u8> data) {
 }
 
 std::vector<u8> RESTBL::ToBinary() {
-  // serialize map
-  return {};
+  Header header{.magic = restbl::Magic,
+                .unknown_1 = m_unknown_1,
+                .unknown_2 = m_unknown_2,
+                .crc_table_num = static_cast<u32>(m_crc_table.size()),
+                .name_table_num = static_cast<u32>(m_name_table.size())};
+
+  exio::BinaryWriter writer{exio::Endianness::Little};
+  writer.Write(header);
+
+  for (const auto [hash, size] : m_crc_table) {
+    writer.Write(restbl::HashEntry{.hash = hash, .size = size});
+  }
+
+  for (const auto [name, size] : m_name_table) {
+    restbl::NameEntry entry{.size = size};
+    strcpy(entry.name, name.c_str());
+    writer.Write(entry);
+  }
+
+  return writer.Finalize();
 }
 
 }  // namespace restbl
