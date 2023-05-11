9a128232e4fc1d51a0a379c8ddc79f0200c50f01
ArchLeaders <archparliament@gmail.com>
2023-05-10 18:45:54 -0700

    Update tests

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,9 +1,16 @@
-#include <restbl/restbl.h>
 #include <iostream>
+#include <restbl/restbl.h>
 
 #include "utils/file_util.h"
 
 int main(int argc, char** argv) {
   std::cout << "[c++] Init Testing" << std::endl;
   const auto data = file::util::ReadAllBytes(argv[1]);
+  oepd::restbl::RESTBL restbl{data};
+
+  for (const auto [name, size] : restbl.m_name_table) {
+    std::cout << name << std::endl;
+  }
+
+  std::cout << "[c++] End Testing" << std::endl;
 }
